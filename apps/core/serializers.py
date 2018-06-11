44a28af3978a8d2ec4f2a820f84aa34ab25782ee
Sidnei Pereira <sidi.jp@gmail.com>
2018-06-11 10:59:24 -0300

    Add category optionally passing a parent category through API.

--- apps/core/serializers.py
+++ apps/core/serializers.py
@@ -1,15 +1,30 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
+from treebeard.mp_tree import MP_AddChildHandler, MP_AddRootHandler
 
 from .models import Article, Category
 
 
-class ArticleSerializer(ModelSerializer):
+class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
         fields = '__all__'
 
 
-class CategorySerializer(ModelSerializer):
+class CategorySerializer(serializers.ModelSerializer):
+    parent = serializers.CharField(required=False)
+
     class Meta:
         model = Category
         exclude = ('path', 'depth', 'numchild',)
+
+    def validate_parent(self, value):
+        try:
+            return Category.objects.get(pk=value)
+        except Category.DoesNotExist:
+            raise serializers.ValidationError('Parent category does exist.')
+
+    def save(self, **kwargs):
+        if 'parent' in self._validated_data:
+            parent = self._validated_data['parent']
+            return MP_AddChildHandler(parent, **self._validated_data).process()
+        return MP_AddRootHandler(Category, **self._validated_data).process()

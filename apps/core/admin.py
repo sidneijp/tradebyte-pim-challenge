d250faf527f94a3ab386b161fccc5c6fd352f18c
Sidnei Pereira <sidi.jp@gmail.com>
2018-06-11 07:57:38 -0300

    Design models.

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from treebeard.admin import TreeAdmin
+from treebeard.forms import movenodeform_factory
 
-# Register your models here.
+from .models import Article, Category
+
+
+class CategoryAdmin(TreeAdmin):
+    form = movenodeform_factory(Category)
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    filter_horizontal = ('categories',)
+
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Category, CategoryAdmin)
